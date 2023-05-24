692215d5666d3a020ed31d64176a697f2d357dce
lapiduzzz <edwardfaraday27@gmail.com>
2023-05-24 11:56:28 +0300

    js refactoring

--- src/js/index.js
+++ src/js/index.js
@@ -147,7 +147,6 @@ const setTabletSettings = () => {
 const setMobileSettings = () => {
     setTabletHeader()
     setTabletThemeToggle()
-    mobileSelectClasses()
 }
 
 
--- src/js/themeToggle.js
+++ src/js/themeToggle.js
@@ -1,18 +1,41 @@
 // theme goggle
 const themeToggle = () => {
     $('.theme_toggle').change(function () {
+        const root = $(':root')
+
         if ($(this).is(':checked')) {
+            root.css('--backgroundColor', 'var(--darkBackground)');
+            root.css('--mainPageHeaderGradient1', 'var(--textGradientDark1)');
+            root.css('--mainPageHeaderGradient2', 'var(--textGradientDark2)');
+            root.css('--themeColor', 'var(--dark)');
+            root.css('--theme', 'light');
+            root.css('--borderColor', 'var(--light)');
+            root.css('--titleColor', 'white');
+            root.css('--textColor', 'var(--textColorDark)');
+            root.css('--objectBackground', 'var(--dark2)');
+            root.css('--objectBackground2', 'var(--dark2)');
+
+
             $('.theme_toggle').each(function () {
                 $(this).prop('checked', true)
                 $(this).addClass('dark')
             })
-            $(':root').css('--theme', 'var(--dark)');
         } else {
             $('.theme_toggle').each(function () {
                 $(this).prop('checked', false)
                 $(this).removeClass('dark')
             })
-            $(':root').css('--theme', 'var(--light)');
+            root.css('--backgroundColor', 'var(--lightBackground)');
+            root.css('--mainPageHeaderGradient1', 'var(--textGradientLight1)');
+            root.css('--mainPageHeaderGradient2', 'var(--textGradientLight2)');
+            root.css('--themeColor', 'var(--light)');
+            root.css('--theme', 'dark');
+            root.css('--borderColor', 'black');
+            root.css('--titleColor', 'black');
+            root.css('--textColor', 'var(--textColorLight)');
+            root.css('--objectBackground', 'var(--light)');
+            root.css('--objectBackground2', 'white');
+
         }
     });
 }
